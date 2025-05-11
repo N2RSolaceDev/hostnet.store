02daa838039f2ea104943c0dcebb56ddb9687579
Solace <monkdev9@gmail.com>
2025-05-11 23:21:56 +0100

    Delete components directory

--- components/ArticleRenderer.tsx
+++
@@ -1,8 +0,0 @@
-export default function ArticleRenderer({ content }: { content: string }) {
-  return (
-    <div
-      className="prose max-w-none prose-a:text-blue-600 hover:prose-a:underline"
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
-  );
-}
